3f2c6606108e97f3ff5bd1928eb32b759d1439ac
shabi-x <2936670268@qq.com>
2024-06-03 13:12:24 +0800

    配置postcss taillwindcss插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
 import path from "path";
 
 const variablePath = normalizePath(path.resolve("./src/variables.scss"));
@@ -11,6 +12,13 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `@import"${variablePath}"`,
+      },
+      postcss: {
+        plugins: [
+          autoprefixer({
+            overrideBrowserslist: ["Chrome >= 40", "ff>31", "ie>11"],
+          }),
+        ],
       },
     },
   },
